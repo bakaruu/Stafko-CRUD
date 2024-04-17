a5938e5bde7e9dbb0a8e0b865931e97236d5a39d
Alvaro Castro <arucastro@gmail.com>
2024-04-17 22:15:26 +0200

    Cors test ip range

--- Backend/api/src/main.ts
+++ Backend/api/src/main.ts
@@ -5,10 +5,23 @@ import { ValidationPipe } from '@nestjs/common'; // Import the ValidationPipe cl
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  
+
   app.enableCors({
     origin: 'http://localhost:5173',
   });
   await app.listen(3000);
+
+  
+  // app.enableCors({
+  //   origin: function (origin, callback) {
+  //     const port = Number(origin.split(':')[2]);
+  //     if (port >= 5173 && port <= 5200) {
+  //       callback(null, true)
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'))
+  //     }
+  //   }
+  // });
+  // await app.listen(3000);
 }
 bootstrap();
