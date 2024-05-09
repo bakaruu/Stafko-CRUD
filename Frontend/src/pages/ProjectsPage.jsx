87551f3c147fc7748e8cf4ec607d36daf371ae5f
bakaruu <arucastro@gmail.com>
2024-05-09 12:43:57 +0200

    Project table name changed ```

--- Frontend/src/pages/ProjectsPage.jsx
+++ Frontend/src/pages/ProjectsPage.jsx
@@ -1,12 +1,16 @@
 
 import AdminNavBar from "../components/navbar/AdminNavbar";
 import Footer from "../components/footer/Footer";
+import ProjectTable from "../components/project/ProjectTable";
 
 const ProjectsPage = () => {
   return (
     <div style={{ position: "relative", minHeight: "100vh" }}>
       <AdminNavBar style={{ zIndex: 1 }} /> {/* Asegúrate de establecer un z-index para el SideBar */}
       
+      <div>
+        <ProjectTable />
+      </div>
       <div >
         
         <Footer />
