e9aba9c925db771d152891993900312d3f0ae980
bakaruu <arucastro@gmail.com>
2024-05-10 13:49:26 +0200

    select customer dropdown fixed

--- Frontend/src/components/project/ProjectForm.jsx
+++ Frontend/src/components/project/ProjectForm.jsx
@@ -1,25 +1,18 @@
 import { useState } from 'react';
+import Select from 'react-select';
 
 
 const ProjectForm = () => {
     const [manager, setManager] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
     const [customer, setCustomer] = useState('');
 
-    const handleSearch = async (event) => {
-        const { value } = event.target;
-        setManager(value);
 
-        // if (value.length > 2) {
-        //     const results = await searchStaff(value);
-        //     setSearchResults(results);
-        // } else {
-        //     setSearchResults([]);
-        // }
-    };
 
+    
     const managers = ['Manager 1', 'Manager 2', 'Manager 3']; // reemplaza con datos reales
-    const customers = ['Customer 1', 'Customer 2', 'Customer 3']; // reemplaza con datos reales
+    const managerOptions = managers.map((manager) => ({ value: manager, label: manager }));
+    const customers = ['Customer 1', 'Customer 2', 'Customer 3', 'Beebit', 'Civica', 'Unit4']; // reemplaza con datos reales
+    const customerOptions = customers.map((customer) => ({ value: customer, label: customer }));
 
     return (
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -33,12 +26,15 @@ const ProjectForm = () => {
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="companyName">
                     Customer
                 </label>
-                <select value={customer} onChange={(e) => setCustomer(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-                    {customers.map((customer, index) => (
-                        <option key={index} value={customer}>{customer}</option>
-                    ))}
-                </select>
-                </div>
+                <Select
+                    value={customerOptions.find(option => option.value === customer)}
+                    onChange={(option) => setCustomer(option.value)}
+                    options={customerOptions}
+                    isSearchable={true}
+                    menuPlacement="auto"
+                    maxMenuHeight={120} // ajusta este valor para cambiar la altura máxima del menú desplegable
+                />
+            </div>
             <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="projectImage">
                     Photo
@@ -55,11 +51,14 @@ const ProjectForm = () => {
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="projectManager">
                     Project Manager
                 </label>
-                <select value={manager} onChange={(e) => setManager(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-                    {managers.map((manager, index) => (
-                        <option key={index} value={manager}>{manager}</option>
-                    ))}
-                </select>
+                <Select
+                    value={managerOptions.find(option => option.value === manager)}
+                    onChange={(option) => setManager(option.value)}
+                    options={managerOptions}
+                    isSearchable={true}
+                    menuPlacement="auto"
+                    maxMenuHeight={120} // ajusta este valor para cambiar la altura máxima del menú desplegable
+                />
             </div>
             <div className="flex items-center justify-between">
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
