b7a1e1f3b3fa0a9f385d54f68305ea5c0555c8dd
bakaruu <arucastro@gmail.com>
2024-05-13 14:42:42 +0200

    Project info fixed

--- Frontend/src/components/project-profile/ProjectProfile.jsx
+++ Frontend/src/components/project-profile/ProjectProfile.jsx
@@ -15,7 +15,7 @@ const ProjectProfile = () => {
                 {/* <!-- End of profile card --> */}
                 <div className="my-4"></div>
                 {/* <!-- Friends card --> */}
-                {/* <div className="bg-white p-3 hover:shadow">
+                <div className="bg-white p-3 mb-14 hover:shadow">
                     <div className="flex items-center space-x-3 font-semibold text-gray-900 text-xl leading-8">
                         <span className="text-green-500">
                             <svg className="h-5 fill-current" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -24,7 +24,7 @@ const ProjectProfile = () => {
                                     d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                             </svg>
                         </span>
-                        <span>Similar Profiles</span>
+                        <span>Staff</span>
                     </div>
                     <div className="grid grid-cols-3">
                         <div className="text-center my-2">
@@ -52,7 +52,7 @@ const ProjectProfile = () => {
                             <a href="#" className="text-main-color">Casey</a>
                         </div>
                     </div>
-                </div> */}
+                </div>
                 {/* <!-- End of friends card --> */}
             </div>
             {/* <!-- Right Side --> */}
